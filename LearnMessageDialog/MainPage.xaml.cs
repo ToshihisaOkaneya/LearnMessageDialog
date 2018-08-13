aaef4bc49057e616c903c7a3473699c62a1cc731
maeda <tankmaeda@gmail.com>
2018-08-13 20:48:22 +0900

    Added MessageDialog and ContentDialog.

--- LearnMessageDialog/MainPage.xaml.cs
+++ LearnMessageDialog/MainPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Diagnostics;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +16,58 @@ namespace LearnMessageDialog
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ShowMessageDialog_Click(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog("MessageDialog content", "Title").ShowAsync();
+        }
+
+        private async void ShowContentDialog_Click(object sender, RoutedEventArgs e)
+        {
+            var contentDialog = new ContentDialog
+            {
+                Title             = "Title",
+                Content           = "ContentDialog content",
+                PrimaryButtonText = "PrimaryButtonText"
+            };
+            await contentDialog.ShowAsync();
+        }
+
+        private async void ShowMessageDialogWithQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog =  new MessageDialog("Answer yes or no.", "Question");
+            messageDialog.Commands.Add(new UICommand("Yes"));
+            messageDialog.Commands.Add(new UICommand("No"));
+            var response = await messageDialog.ShowAsync();
+            if (response.Label == "Yes")
+            {
+                Debug.WriteLine("Answer is yes.");
+            }
+            else if (response.Label == "No")
+            {
+                Debug.WriteLine("Answer is no.");
+            }
+        }
+
+        private async void ShowContentDialogWithQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            var contentDialog = new ContentDialog
+            {
+                Title               = "Title",
+                Content             = "Answer yes or no.",
+                PrimaryButtonText   = "Yes",
+                SecondaryButtonText = "No",
+            };
+            var response = await contentDialog.ShowAsync();
+            if (response == ContentDialogResult.Primary)
+            {
+                Debug.WriteLine("Answer is yes.");
+            }
+            else if (response == ContentDialogResult.Secondary)
+            {
+                Debug.WriteLine("Answer is no.");
+            }
         }
     }
 }
